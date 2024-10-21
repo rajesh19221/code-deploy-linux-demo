32eec0dc29a66657d729799e9e8c9570c757a424
rajesh <rajesh.rawat@gmail.com>
2024-10-21 18:56:32 +0530

    commit1

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,3 +4,8 @@ echo "Running before install scripts"
 # Example commands:
 sudo apt-get update
 sudo apt-get install -y package_name
+
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+set -x  # Enable debugging
+
