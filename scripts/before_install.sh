c9a1bf68001380a66a4c6713b08a411662e95deb
rajesh <rajesh.rawat@gmail.com>
2024-10-21 19:28:04 +0530

    sixxxxx

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-sudo su -
-echo "Running before install scripts"
-# Example commands:
-sudo cd /var/www/html
-sudo rm -rf *
-
-#!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status
 set -x  # Enable debugging
 
+echo "Running before install scripts"
+# Clear the target directory before copying new files
+sudo rm -rf /var/www/html/*
